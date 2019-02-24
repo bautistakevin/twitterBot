08f73f63b9768eb77658c93a2624fb689a7cffbc
Kevin Bautista <41712873+bautistakevin@users.noreply.github.com>
2019-02-24 11:29:19 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,60 @@
+console.log("the bot is starting!");
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+var T = new Twit(config);
+
+//setting up a user stream
+var stream = T.stream('user');
+
+//anytime someone follows me, function is called
+stream.on('follow', followed);
+
+function followed(event){
+    console.log("follow event!");
+    var name = event.source.name;
+    var screenName = event.source.screen_name;
+    tweetIt('@' + screenName + ' thanks for following me! #twitterbot from Node.js');
+}
+
+//function call to make bot post
+//tweetIt();
+
+//setInterval calls the function and sets it in miliseconds (1000*20 = 20 seconds)
+//setInterval(tweetIt, 1000*60)
+
+function tweetIt(txt){
+    
+    var tweet = {
+        status: txt
+    }
+
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err, data, response){
+    if(err){
+        console.log("error posting tweet");
+        }
+        else {
+            console.log("post succesful!");
+        }
+    }
+}
+
+// var params = {
+//   q: 'kevin bautista',
+//   count: 2
+// }
+
+// T.get('search/tweets', params, gotData);
+
+// function gotData(err, data, response) {
+//     var tweets = data.statuses;
+//     for(var i=0; i < tweets.length; i++){
+//         console.log(tweets[i].text);
+//     }
+
+
+
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    consumer_key:         'CDqBy3m7PFo5wrEJJg4jFkwEx',
+    consumer_secret:      'l7GxspKlKy0JUDvufRRwe8TliWZ7NcQu9vgEjavx9dEehcS2lB',
+    access_token:         '1099351764187447296-HezNuYvdgA0FIogohhZVZJdDLTHBc0',
+    access_token_secret:  'cAtINz6SMw0m8oJ1uLRCyxhC1DFcz7ri3ajm5v3DiYVPN',
+};
